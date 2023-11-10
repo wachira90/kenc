49014294b9b253616ca13943d43a72564b3dfc7d
Wachira Duangdee <wcrcorp@gmail.com>
2023-11-10 11:11:42 +0700

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -3,9 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
-
 	"github.com/spf13/cobra"
-
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
 )
@@ -13,10 +11,10 @@ import (
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Use:   "k64",
-	Short: "K64 is a helper that help you convert secret string in K8S yaml into base64.",
+	Use:   "kenc",
+	Short: "kenc is a helper that help you convert secret string in K8S yaml into base64.",
 	Long: `When creating a secret for Kubernetes using yaml file. The secret need to be in base64 format.
-	k64 is designed to help you convert original string to base64 string.`,
+	kenc is designed to help you convert original string to base64 string.`,
 }
 
 func Execute() {
@@ -28,7 +26,7 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.k64.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kenc.yaml)")
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
